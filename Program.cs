417ee4ec459b863be2ad6e799e661e35e3031dd5
drmeth <mathias.schustereder@hotmail.com>
2020-02-09 18:26:17 +0100

    first and last

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Even_or_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a Number: ");
+            string myNumValue = Console.ReadLine();
+            int myNum = Convert.ToInt32(myNumValue);
+
+            if (myNum % 2 == 0)
+            {
+                Console.WriteLine("This Number is even!");
+            }
+            if (myNum % 2 == 1)
+            {
+                Console.WriteLine("This Number is odd!");
+            }
+
+            //keep window open
+            Console.ReadLine();
+        }
+    }
+}
